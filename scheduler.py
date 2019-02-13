47bfd35951d3e4d99757a27df80e8aac4563ca17
Kody Sanchez <kodysanchez@conservice.com>
2019-02-13 13:55:08 -0700

    Removed the optimizer classed and placed its method in the scheduler.

--- scheduler.py
+++ scheduler.py
@@ -1,5 +1,8 @@
-from stop import Start, Charger, Destination
-from optimizer import Optimizer
+from battery import NissanLeafBattery
+from ev_trip_scheduler_env import EvTripScheduleEnvironment
+from stop import Charger, Destination, Start
+from value_interation_agent import ValueIterationAgent
+
 
 class Trip:
     """ An object used to hold information about a particular trip. 
@@ -13,8 +16,6 @@ class Trip:
 class Scheduler:
     """ Provides methods to schedule the charging locations several EV trip routes. 
     """
-    def __init__(self):
-        self.Optimizer = Optimizer()
 
     def ScheduleRoutes(self, routes, isPrintStats=False):
         """ Given a list of routes, find the optimal schedule for each on and return them. 
@@ -28,7 +29,7 @@ class Scheduler:
 
         schedules = []
         for trip in scheduledRoutes:
-            schedules.append(self.Optimizer.OptimizeRoute(trip.Name, trip.Route, trip.Battery, trip.TripTime, isPrintStats))
+            schedules.append(self.OptimizeRoute(trip.Name, trip.Route, trip.Battery, trip.TripTime, isPrintStats))
 
         return schedules
 
@@ -62,3 +63,31 @@ class Scheduler:
 
         # Return the trip object. 
         return Trip(name, trip, tripTime, battery)
+
+    def OptimizeRoute(self, routeName, route, battery, expectedTimeToDestination, isPrintStats):
+        """
+            Given a route, a car battery, and an expected arrival time, find the optimal charging location schedule and return it. 
+
+            Keyword arguments:
+
+            route -- A list of possible charging points during the trip. Each charging point contains information about that location, how long it took to get there 
+                    from the previous location, and how much energy was expended to reach the location. 
+            battery -- A model of the EV car battery for calculating charging and discharging rates. 
+            expectedTimeToDestination -- The expected time to reach the destination. 
+            isPrintStats -- Boolean value that triggers printing the optimizer output to the console. 
+        """
+
+        environment = EvTripScheduleEnvironment(route, expectedTimeToDestination, battery)
+
+        # Build the value table
+        agent = ValueIterationAgent(environment)
+        agent.PerformValueIteration(isPrintStats)
+        agent.FindOptimalPolicy(isPrintStats)
+
+        # Evaluate
+        agent.EvaluatePolicy(1, False, isPrintStats)
+
+        # Display Graphs
+        agent.DisplayEvaluationGraphs(routeName)
+
+        return agent.GetSchedule()
